62e3b68f7bf6881c69066e46454c4256778c8714
Arieh Glazer <arieh.glazer@gmail.com>
2012-03-20 09:24:28 +0200

    first tests

--- tests/src/AlertSpec.js
+++ tests/src/AlertSpec.js
@@ -1,3 +1,14 @@
-define("Alert",function(){
+describe("Alert",function(){
+
+    var alrt;
+
+    it ("Should create an alert", function(){
+        var a = new Alert();
+
+        expect(a instanceof Alert).toEqual(true, "a should be an alert");
+        expect(a.element).toEqual(true, "a should have a root element");
+        expect(a.element.hasClass('Alert')).toEqual(true, "a should have a class named 'Alert'");
+    });
     
+
 });
