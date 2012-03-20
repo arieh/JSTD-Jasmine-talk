7cd407f248c2a2016d74c26fcf045eb9f0a9fa34
Arieh Glazer <arieh.glazer@gmail.com>
2012-03-20 09:53:18 +0200

    working on IE

--- tests/src/AlertSpec.js
+++ tests/src/AlertSpec.js
@@ -1,5 +1,14 @@
 describe("Alert",function(){
 
+    function hasClass(el,name){
+        var arr;
+
+        if (el.classList) return el.classList.contains(name);
+
+        arr = el.className.split(' ');
+        return arr.indexOf(name) > -1;
+    }
+
     var alrt;
 
     it ("Should create an alert", function(){
@@ -7,20 +16,20 @@ describe("Alert",function(){
 
         expect(a instanceof Alert).toEqual(true, "a should be an alert");
         expect(a.element).toBeTruthy(true, "a should have a root element");
-        expect(a.element.classList.contains('Alert')).toEqual(true, "a should have a class named 'Alert'");
+        expect(hasClass(a.element,'Alert')).toEqual(true, "a should have a class named 'Alert'");
     });
     
     it ("Should open when calling show method", function(){
         var a = new Alert(), flag;
 
-        expect(a.element.classList.contains('close')).toEqual(true, "element should be hidden by default");
-        expect(a.element.classList.contains('open')).toEqual(false, "element should not contain an open class");
+        expect(hasClass(a.element,'close')).toEqual(true, "element should be hidden by default");
+        expect(hasClass(a.element,'open')).toEqual(false, "element should not contain an open class");
 
         a.addEvent('show',function(){
             flag = true;
 
-            expect(a.element.classList.contains('close')).toEqual(false, "element should not have a close class when opened");
-            expect(a.element.classList.contains('open')).toEqual(true, "element should contain an open class when opened");
+            expect(hasClass(a.element,'close')).toEqual(false, "element should not have a close class when opened");
+            expect(hasClass(a.element,'open')).toEqual(true, "element should contain an open class when opened");
         });
         
         a.show();                       
