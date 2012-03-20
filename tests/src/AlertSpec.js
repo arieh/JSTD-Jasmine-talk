90545fc3498634666c20bcad145638e0ece15897
Arieh Glazer <arieh.glazer@gmail.com>
2012-03-20 09:30:15 +0200

    tests formatted propely

--- tests/src/AlertSpec.js
+++ tests/src/AlertSpec.js
@@ -6,8 +6,8 @@ describe("Alert",function(){
         var a = new Alert();
 
         expect(a instanceof Alert).toEqual(true, "a should be an alert");
-        expect(a.element).toEqual(true, "a should have a root element");
-        expect(a.element.hasClass('Alert')).toEqual(true, "a should have a class named 'Alert'");
+        expect(a.element).toBeTruthy(true, "a should have a root element");
+        expect(a.element.classList.contains('Alert')).toEqual(true, "a should have a class named 'Alert'");
     });
     
 
