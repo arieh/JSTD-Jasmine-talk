9ca449c27f5fe0f53a03a21455cfd259ea8f9343
Arieh Glazer <arieh.glazer@gmail.com>
2012-03-20 09:35:37 +0200

    testing show

--- tests/src/AlertSpec.js
+++ tests/src/AlertSpec.js
@@ -10,5 +10,17 @@ describe("Alert",function(){
         expect(a.element.classList.contains('Alert')).toEqual(true, "a should have a class named 'Alert'");
     });
     
+    it ("Should open when calling show method", function(){
+        var a = new Alert();
+
+        expect(a.element.classList.contains('close')).toEqual(true, "element should be hidden by default");
+        expect(a.element.classList.contains('open')).toEqual(false, "element should not contain an open class");
+        
+        a.show();                       
+
+        expect(a.element.classList.contains('close')).toEqual(false, "element should not have a close class when opened");
+        expect(a.element.classList.contains('open')).toEqual(true, "element should contain an open class when opened");
+    });
+    
 
 });
